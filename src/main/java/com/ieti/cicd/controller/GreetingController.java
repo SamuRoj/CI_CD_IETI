e9a2002531c3bd47a684327d70280b30fc4935f3
Samuel Rojas <samu.02309@gmail.com>
2025-02-23 10:07:51 -0500

    Greeting Controller and Service

---
+++ src/main/java/com/ieti/cicd/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.ieti.cicd.controller;
+
+import com.ieti.cicd.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class GreetingController {
+
+    private GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> greeting() {
+        String response = greetingService.greeting();
+        return ResponseEntity.ok(response);
+    }
+}
